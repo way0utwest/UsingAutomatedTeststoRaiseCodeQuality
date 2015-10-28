f178bf08a549f7441045ce61a9286fc14f8b6c0e
Steve.Aristotle <way0utwest@hotmail.com>
2015-10-28 16:48:25 -0700

    header changes

--- 41-Heuristics.sql
+++ 41-Heuristics.sql
@@ -1,6 +1,13 @@
 /*
 Using Automated Testing to Raise Code Quality - 41-Heuristics
 
+
+Copyright 2015, Sebastian Meine and Steve Jones
+
+This code is provided as is for demonstration purposes. It may not be suitable for
+your environment. Please test this on your own systems. This code may not be republished 
+or redistributed by anyone without permission.
+You are free to use this code inside of your own organization.
 */
 
 -- We have a function for discounts, UF_CalcDiscountForSale
